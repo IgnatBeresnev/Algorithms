7b6d927624fa959ac4b10ef654b1c926175e06ed
Ignat Beresnev <ignat@beresnev.me>
2017-03-17 00:40:05 +0300

    Improve TextJustification

--- src/main/java/me/beresnev/algorithms/text/TextJustification.java
+++ src/main/java/me/beresnev/algorithms/text/TextJustification.java
@@ -1,14 +1,16 @@
 package me.beresnev.algorithms.text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ignat Beresnev
- * @version 1.0
+ * @version 1.1
  * @since 15.03.17.
  */
 public class TextJustification {
 
-    private static final int PAGE_WIDTH = 70;
-    private static boolean isLast = false;
+    private static final int PAGE_WIDTH = 40;
 
     /**
      * Work in progress
@@ -16,44 +18,45 @@ public class TextJustification {
     private TextJustification() {
     }
 
-    public static void printLines(String line) {
-        isLast = false;
+    public static List<String> getLines(String line) {
+        List<String> lines = new ArrayList<>();
         String[] words = line.split("\\s+");
+        int[] wordsLength = preCountPrefixLength(words);
         int current = 0;
         while (current < words.length) {
-            int finish = DP(words, current);
+            int finish = DP(words, wordsLength, current);
             if (finish > words.length - 1) {
                 finish = words.length;
             }
-            System.out.println(toPrint(words, current, finish));
+            lines.add(getLineFromWords(words, wordsLength, current, finish));
             current = finish;
         }
+        return lines;
     }
 
     // i - starting point, doesn't change
     // k - floating, going from i++ to find the end of current line
-    private static int DP(String[] arr, int i) {
+    private static int DP(String[] arr, int[] len, int i) {
         int min = Integer.MAX_VALUE;
         int minPointer = Integer.MAX_VALUE;
         for (int k = i + 1; k < arr.length; k++) {
-            int badness = badness(arr, i, k, PAGE_WIDTH);
+            int badness = badness(len, i, k, PAGE_WIDTH);
             if (badness == Integer.MAX_VALUE) {
                 break;
             } else if (badness < min) {
                 min = badness;
                 minPointer = k;
-            }
+            } // TODO: if badness max value then k is already +1
         }
 
         if (minPointer == arr.length - 1) {
             minPointer = arr.length;
-            isLast = true;
         }
         return minPointer;
     }
 
-    private static int badness(String[] arr, int i, int j, int pageWidth) {
-        int total = countChars(arr, i, j);
+    private static int badness(int[] len, int i, int j, int pageWidth) {
+        int total = getLengthSum(len, i, j);
         if (total > pageWidth) {
             return Integer.MAX_VALUE;
         } else {
@@ -61,30 +64,41 @@ public class TextJustification {
         }
     }
 
-    // TODO: Pre-count the lengths of all string so that later length(j, i) - O(1)
-    private static int countChars(String[] arr, int start, int finish) {
-        int characters = 0;
-        int needMinSpaces = 0;
-        for (int i = start; i < finish; i++) {
-            characters += arr[i].length();
-            needMinSpaces++;
-        }
-        return characters + (needMinSpaces - 1); // OR needMinSpaces - 1
-    }
-
-    private static String toPrint(String[] arr, int from, int to) {
+    private static String getLineFromWords(String[] arr, int[] len, int from, int to) {
         StringBuilder builder = new StringBuilder();
-        int charsTotal = countChars(arr, from, to);
-        int spaces = PAGE_WIDTH - charsTotal;
+        int charsTotal = getLengthSum(len, from, to);
+        int needSpaces = PAGE_WIDTH - charsTotal;
+
         for (int i = from; i < to; i++) {
-            builder.append(arr[i]).append(' '); // TODO: Remove flag isLast
-            if (((to - from) <= spaces) && !isLast) { // TODO: Optimize additional spaces based on words/length
-                builder.append("  ");
-            } else if (spaces > 0 && !isLast) {
-                builder.append(' ');
+            builder.append(arr[i]).append(' ');
+
+            if (to != arr.length) {
+                if (((to - from) <= needSpaces)) {
+                    // TODO: Calculate required #spaces per space based on neededSpaces and #words.
+                    builder.append("  ");
+                } else if (needSpaces > 0) {
+                    builder.append(' ');
+                }
+                needSpaces--;
             }
-            spaces--;
         }
         return builder.toString().trim();
+    }
+
+    private static int getLengthSum(int[] arr, int from, int to) {
+        to -= 1;
+        return from != 0 ? arr[to] - arr[from - 1] : arr[to];
+    }
+
+    private static int[] preCountPrefixLength(String[] arr) {
+        int[] sum = new int[arr.length];
+        int sumCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            sumCount += word.length() + 1;
+            sum[i] = sumCount;
+        }
+        sum[arr.length - 1] -= 1;
+        return sum;
     }
 }
